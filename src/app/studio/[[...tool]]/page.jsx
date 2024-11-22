4984bd5ec0e3c333dfa74ea4ea52e16cdb06e141
Akpcodes636 <ewherheakpesiri@gmail.com>
2024-11-22 17:30:57 +0100

    updates

--- src/app/studio/[[...tool]]/page.jsx
+++ src/app/studio/[[...tool]]/page.jsx
@@ -8,3 +8,7 @@ export { metadata, viewport } from "next-sanity/studio";
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
+
+// akpcodes636
+// 3129675948Akp
+// git remote set-url origin https://akpcodes636:token@github.com/Akpcodes636/cryptoCommunity.git
